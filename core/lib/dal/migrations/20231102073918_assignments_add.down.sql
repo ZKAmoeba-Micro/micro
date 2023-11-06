5a56da7f943b64f92da5513cdf7deca01ae7dd1d
barnett-1 <lrmlxl@outlook.com>
2023-11-06 15:34:58 +0800

    Add a table for migrations series

--- core/lib/dal/migrations/20231102073918_assignments_add.down.sql
+++ core/lib/dal/migrations/20231102073918_assignments_add.down.sql
@@ -1,3 +1,13 @@
 DROP INDEX IF EXISTS assignments_un_address_l1batch;
 DROP INDEX IF EXISTS assignments_idx_status;
 DROP TABLE IF EXISTS assignments;
+
+DROP INDEX IF EXISTS assignment_user_summary_pkey;
+DROP TABLE IF EXISTS assignment_user_summary;
+
+
+DROP INDEX IF EXISTS assignment_rewards_pkey;
+DROP TABLE IF EXISTS assignment_rewards;
+
+DROP INDEX IF EXISTS assignment_penalties_detail_pkey;
+DROP TABLE IF EXISTS assignment_penalties_detail;
--- core/lib/dal/migrations/20231102073918_assignments_add.up.sql
+++ core/lib/dal/migrations/20231102073918_assignments_add.up.sql
@@ -11,3 +11,46 @@ CREATE TABLE assignments (
 
 CREATE UNIQUE INDEX assignments_un_address_l1batch ON assignments (verification_address, l1_batch_number);
 CREATE INDEX assignments_idx_status ON assignments (status);
+
+
+
+CREATE TABLE assignment_user_summary (
+    verification_address bytea NOT NULL,
+    status text NOT NULL,
+    dissemination_num bigint NOT NULL,
+    completion_num bigint NOT NULL,
+    timeout_num bigint NOT NULL,
+    score numeric(10, 2) NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    update_at timestamp without time zone NOT NULL
+  );
+
+ALTER TABLE assignment_user_summary ADD CONSTRAINT assignment_user_summary_pkey PRIMARY KEY (verification_address);
+
+
+
+CREATE TABLE assignment_rewards (
+    id serial NOT NULL,
+    verification_address bytea NOT NULL,
+    sender_address bytea NOT NULL,
+    rewards numeric(80, 0) NULL,
+    receive_status text NOT NULL,
+    receive_time_at timestamp without time zone NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    update_at timestamp without time zone NOT NULL DEFAULT now()
+  );
+
+ALTER TABLE assignment_rewards ADD CONSTRAINT assignment_rewards_pkey PRIMARY KEY (id);
+
+
+
+CREATE TABLE assignment_penalties_detail (
+    id serial NOT NULL,
+    verification_address bytea NOT NULL,
+    penalties_num numeric(80, 0) NOT NULL DEFAULT 0,
+    type character varying(20) NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    update_at timestamp without time zone NOT NULL DEFAULT now()
+  );
+
+ALTER TABLE assignment_penalties_detail ADD CONSTRAINT assignment_penalties_detail_pkey PRIMARY KEY (id);
